553a9b704515934789f6a7f654b6f353b67e23f3
darkluc05 <34852@ma-web.nl>
2023-12-13 15:19:54 +0100

    javascript and json first data for frontpage

--- code/js/main.js
+++ code/js/main.js
@@ -16,11 +16,46 @@ class GetData {
     }
 }
 
+class Renderer {
+    render(whereToRender, whatToRender) {
+        document.querySelector(whereToRender).appendChild(whatToRender);
+    }
+}
+
+class Main{
+    renderer
+    data
+    constructor(data, renderer){
+        this.renderer = renderer
+        this.data = data
+
+        this.title = document.querySelector(".header__title")
+        this.title.innerText = data.title
+    }
+}
+
+class Info{
+    renderer
+    data
+    constructor(data, renderer){
+        this.renderer = renderer
+        this.data = data
+
+        this.title = document.querySelector(".info__title")
+        this.title.innerText = data.title
+    }
+}
+
 class App{
     api
+    info
     constructor(){
-        this.api = new GetData("../data/data.json")
-        this.api.getJson().then((data) => {});
+        this.api = new GetData("./data/data.json")
+        this.renderer = new Renderer()
+        this.api.getJson().then((data) => {
+            this.main = new Main(data.algemeen[0], this.renderer)
+            this.info = new Info(data.info[0], this.renderer)
+        });
     }
 }
 
