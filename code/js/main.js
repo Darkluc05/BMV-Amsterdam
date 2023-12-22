5444dc375f9d0a44149318b5f99d1f6cd7838bd4
Luc <34852@ma-web.nl>
2023-12-22 10:25:26 +0100

    header and info progress

--- code/js/main.js
+++ code/js/main.js
@@ -44,10 +44,69 @@ class Info{
         this.title = document.querySelector(`.${className}__title`)
         this.title.innerText = data.title
 
+        this.text = document.querySelector(`.${className}__textTitle`)
+        this.text.innerText = data.textTitle
+
         this.text = document.querySelector(`.${className}__text`)
         this.text.innerText = data.text
     }
 }
+
+class Slideshow {
+    constructor() {
+      this.currentSlideIndex = 0;
+      this.slides = document.querySelectorAll('.projects__project');
+      this.totalSlides = this.slides.length;
+  
+      this.showSlide(this.currentSlideIndex);
+      this.startSlideshow();
+      this.setupButtons();
+    }
+  
+    showSlide(index) {
+      this.slides.forEach((slide, i) => {
+        slide.style.display = i === index ? 'block' : 'none';
+      });
+    }
+  
+    nextSlide() {
+      this.currentSlideIndex = (this.currentSlideIndex + 1) % this.totalSlides;
+      this.showSlide(this.currentSlideIndex);
+    }
+  
+    prevSlide() {
+      this.currentSlideIndex =
+        (this.currentSlideIndex - 1 + this.totalSlides) % this.totalSlides;
+      this.showSlide(this.currentSlideIndex);
+    }
+  
+    startSlideshow() {
+      setInterval(() => {
+        this.nextSlide();
+      }, 10000); // Adjust the interval as needed (in milliseconds)
+    }
+  
+    setupButtons() {
+      const nextButton = document.getElementById('nextButton');
+      const prevButton = document.getElementById('prevButton');
+  
+      if (nextButton && prevButton) {
+        nextButton.addEventListener('click', () => this.nextSlide());
+        prevButton.addEventListener('click', () => this.prevSlide());
+      }
+    }
+  }
+  
+  // Instantiate the Slideshow class when the DOM is ready
+  document.addEventListener('DOMContentLoaded', () => {
+    new Slideshow();
+  });
+  
+  
+  // Instantiate the Slideshow class when the DOM is ready
+document.addEventListener('DOMContentLoaded', () => {
+    new Slideshow();
+});  
 
 class App{
     api
